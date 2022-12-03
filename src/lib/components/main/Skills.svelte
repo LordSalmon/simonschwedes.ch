6ba9309ddcfbf203e2513f1644044c5089e1627b
Simon Schwedes <simon@schwedes.eu>
2022-12-03 12:00:34 +0100

    continued with skills

--- src/lib/components/main/Skills.svelte
+++ src/lib/components/main/Skills.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
     import { client, getURLForSource } from "$lib/sanity/client";
     import { getSkills } from "$lib/sanity/queries";
+    import Title from "$lib/components/styling/Title.svelte";
 
     async function fetchSkills(): Promise<any[]> {
         const skills = await client.fetch(getSkills);
@@ -9,18 +10,25 @@
 </script>
 
 <section>
+    <Title>✌🏻 Cool Technologies:</Title>
     {#await fetchSkills()}
         <p>loading...</p>
     {:then skills}
-        <div class="flex items-center">
+        <div class="grid grid-cols-8">
             {#each skills as skill}
-                <div class="p-2">
+                <a
+                    class="p-2 grid place-items-center"
+                    title={skill.title}
+                    href={skill.url}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
                     <img
                         class="w-10"
                         src={getURLForSource(skill.image).url()}
                         alt="{skill.title}-Logo"
                     />
-                </div>
+                </a>
             {/each}
         </div>
     {/await}
--- src/lib/components/styling/Callout.svelte
+++ src/lib/components/styling/Callout.svelte
@@ -1,3 +1,3 @@
-<div class="box-border p-4 border-l-4 border-l-blue-600">
+<div class="box-border p-4 border-l-4 border-l-blue-600 ">
     <slot />
 </div>
