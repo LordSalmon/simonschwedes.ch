f6c05ba545b36cf5863d9f000353629968d24bed
Simon Schwedes <simon@schwedes.eu>
2024-06-21 18:21:09 +0200

    fix: start with utils

--- src/lib/components/utils/UtilsPreview.svelte
+++ src/lib/components/utils/UtilsPreview.svelte
@@ -4,8 +4,9 @@
     export let util: Util;
 </script>
 
-<div
+<a
     class="bg-slate-400 bg-opacity-50 rounded-lg box-border p-4 shadow-sm transition-all duration-200 hover:bg-opacity-70 cursor-pointer"
+    href={util.url}
 >
     <div>
         <span class="text-lg">{util.title}</span>
@@ -13,4 +14,4 @@
     <div>
         <span>{util.description}</span>
     </div>
-</div>
+</a>
---
+++ src/routes/utils/pwgen/+page.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+    let passwordField;
+    let password = "";
+    let passwordLength = 60;
+    let useNumbers = true;
+    let useUppercase = true;
+    let useSpecialCharacters = true;
+
+    function generatePassword() {
+        const numbers = "0123456789";
+        const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        const lowercase = uppercase.toLowerCase();
+        const specialCharacters = "!@#$%&()_-+={}[];:<>.?|~";
+        const characters = [
+            useNumbers ? numbers : "",
+            useUppercase ? uppercase : "",
+            useSpecialCharacters ? specialCharacters : "",
+            lowercase,
+        ].join("");
+        password = Array(passwordLength)
+            .fill(characters)
+            .map((x) => x[Math.floor(Math.random() * x.length)])
+            .join("");
+    }
+
+    generatePassword();
+
+    function setPassword(e: Event) {
+        password = (e.target as HTMLInputElement).value;
+    }
+</script>
+
+<div class="box-border p-6 flex justify-center items-start">
+    <div>
+        <div
+            class="border-2 rounded border-white box-border py-2 px-4 bg-gray-900 bg-opacity-50"
+        >
+            <input
+                class="bg-transparent outline-none border-0 w-full"
+                bind:this={passwordField}
+                bind:value={password}
+                on:input={setPassword}
+            />
+        </div>
+        <span>{password}</span>
+    </div>
+</div>
