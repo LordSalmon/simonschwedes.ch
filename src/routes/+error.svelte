506ec6cb1fb604daf0300c8115e086df180a7667
Simon Schwedes <simon@schwedes.eu>
2025-11-11 11:58:27 +0100

    debug

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -5,6 +5,6 @@
 </script>
 
 <section class="flex justify-center items-center">
-	das ist ein test
+	das ist ein test 2
 	<img src={astronaut} alt="man who stares into the void" class="h-[75vh]" />
 </section>
