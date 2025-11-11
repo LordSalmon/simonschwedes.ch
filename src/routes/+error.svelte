7ebe600d6d51fcb3183ca4ef2ab667bca59a74f8
Simon Schwedes <simon@schwedes.eu>
2025-11-11 11:56:33 +0100

    current deployment

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
-    import astronaut from "$lib/images/void.svg";
-    export const data = {};
-    export const form = {};
+	import astronaut from '$lib/images/void.svg';
+	export const data = {};
+	export const form = {};
 </script>
 
 <section class="flex justify-center items-center">
-    <img src="{astronaut}" alt="man who stares into the void" class="h-[75vh]">
+	das ist ein test
+	<img src={astronaut} alt="man who stares into the void" class="h-[75vh]" />
 </section>
